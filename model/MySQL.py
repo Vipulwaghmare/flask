4aed916c341e61745765803e3debeff5af0df052
vipul <vipulwaghmare222@gmail.com>
2022-09-12 20:02:11 +0530

    twilio and mysql db added

--- model/MySQL.py
+++ model/MySQL.py
@@ -1,2 +1,45 @@
-from mysql.connector import Error
-from mysql.connector import pooling
+from dataclasses import dataclass
+import mysql.connector
+import json
+from settings.environment import config_folder
+
+class MySQL:
+  def __init__(self, database_name, config_file_name = "db", **opts):
+    self.connection = None
+    self._database = database_name
+    self.load_config(config_file_name)
+    self.create_connection(opts)
+
+  def load_config(self, config_file_name):
+    # config_folder not available in global variables
+    if not 'config_folder' in globals():
+      raise NameError("[MySql] config_folder is not set")
+    file_path = f"settings/{config_folder}/{config_file_name}.json"
+    with open(file_path) as config_file:
+      config_data = json.load(config_file)
+      self._host = config_data["host"]
+      self._user = config_data["user_name"]
+      self._password = config_data["password"]
+
+  def create_connection(self, opts):
+    self.connection = mysql.connector.connect(
+      host = self._host,
+      database = self._database,
+      user = self._user,
+      password = self._password,
+      **opts
+    )
+    self.connection = self.get_connection()
+  
+  def get_connection(self):
+    if not self.connection.is_connected():
+      self.connection.reconnect(attempts = 4, delay = 5)
+    return self.connection
+  
+  def close(self):
+    self.connection.close()
+
+class BaseWithDB:
+  def __init__(self):
+    self._table_name = None
+  
